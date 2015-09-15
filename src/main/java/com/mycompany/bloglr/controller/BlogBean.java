61b1cd58140ce0f6667c63d65cf60fc5909bc5ed
Colin But <colinbut@users.noreply.github.com>
2015-09-15 20:19:38 +0100

    add new post form

---
+++ src/main/java/com/mycompany/bloglr/controller/BlogBean.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.mycompany.bloglr.controller;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+/**
+ * @author colin
+ *
+ */
+@ManagedBean
+@SessionScoped
+public class BlogBean implements Serializable {
+
+	private static final long serialVersionUID = 2112003448015528406L;
+
+	public BlogBean() {
+		
+	}
+}
