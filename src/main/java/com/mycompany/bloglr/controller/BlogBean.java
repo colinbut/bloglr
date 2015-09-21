81affdbe676a088d77a14df32bc71f4977cc0ac9
Colin But <colinbut@users.noreply.github.com>
2015-09-21 21:20:28 +0100

    bloglist main page front end JSF

--- src/main/java/com/mycompany/bloglr/controller/BlogBean.java
+++ src/main/java/com/mycompany/bloglr/controller/BlogBean.java
@@ -6,18 +6,23 @@
 package com.mycompany.bloglr.controller;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
-import javax.ejb.EJB;
 import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.mycompany.bloglr.persister.repository.BlogUserRepository;
-import com.mycompany.bloglr.persister.repository.entities.BlogUserEntity;
+import com.mycompany.bloglr.domain.model.BlogPost;
 
 /**
+ * {@link BlogBean} 
+ * 
  * @author colin
  *
  */
@@ -29,24 +34,85 @@ public class BlogBean implements Serializable {
 
 	final Logger logger = LoggerFactory.getLogger(getClass());
 	
-	@EJB
-	private BlogUserRepository blogUserRepository;
+	private List<BlogPost> blogList = new ArrayList<>();
+	
+	private boolean sortAscending = true;
+	
+	public List<BlogPost> getBlogList() {
+		return blogList;
+	}
+
+	public void setBlogList(List<BlogPost> blogList) {
+		this.blogList = blogList;
+	}
 		
 	public BlogBean() {
+		setupBlogPostStubs();
+	}
+	
+	private void setupBlogPostStubs() {
+		
+		for(int i = 1; i <= 20; i++) {
+			BlogPost blogPost = new BlogPost();
+			blogPost.setPostId(i);
+			blogPost.setTitle("Title");
+			blogPost.setContent("Content");
+			blogPost.setDateCreated(LocalDateTime.now());
+			blogList.add(blogPost);
+		}
 		
 	}
 	
-	public String getUsers() {
-		BlogUserEntity blogUser = blogUserRepository.getUserById(1);
-		logger.info("Getting User: " + blogUser.toString());
+	
+	public String addBlogPost(BlogPost blogPost) {
 		return null;
 	}
 	
-	public BlogUserRepository getBlogUserRepository() {
-		return blogUserRepository;
+	public String deleteBlogPost(BlogPost blogPost) {
+		logger.info("Deleting blog post: " + blogPost);
+		blogList.remove(blogPost);
+		return null;
 	}
+	
+	public String editBlogPost(BlogPost blogPost) {
+		logger.info("Edited blog post: " + blogPost);
+		blogPost.setEditable(true);
+		return null;
+	}
+	
+	public String saveBlogPost() {
+		blogList.stream().forEach(blogPost -> {blogPost.setEditable(false);});
+		return null;
+	}
+	
+	public String sortByPostId() {
+		
+		if(sortAscending) {
+			Collections.sort(blogList, new Comparator<BlogPost>() {
 
-	public void setBlogUserRepository(BlogUserRepository blogUserRepository) {
-		this.blogUserRepository = blogUserRepository;
+				@Override
+				public int compare(BlogPost o1, BlogPost o2) {
+					String bp1 = Integer.toString(o1.getPostId());
+					String bp2 = Integer.toString(o2.getPostId());
+					return bp1.compareTo(bp2);
+				}
+			});
+			sortAscending = false;
+		} else {
+			Collections.sort(blogList, new Comparator<BlogPost>() {
+
+				@Override
+				public int compare(BlogPost o1, BlogPost o2) {
+					String bp1 = Integer.toString(o1.getPostId());
+					String bp2 = Integer.toString(o2.getPostId());
+					return bp2.compareTo(bp1);
+				}
+				
+			});
+			sortAscending = true;
+		}
+		
+		return null;
 	}
+	
 }
--- src/main/java/com/mycompany/bloglr/domain/model/BlogPost.java
+++ src/main/java/com/mycompany/bloglr/domain/model/BlogPost.java
@@ -14,10 +14,71 @@ import java.util.List;
  */
 public class BlogPost {
 
+	private int postId;
 	private String title;
 	private Author author;
 	private LocalDateTime dateCreated;
 	private LocalDateTime dateUpdated;
 	private String content;
 	private List<BlogComment> comments;
+	
+	private boolean editable;
+	
+	public boolean isEditable() {
+		return editable;
+	}
+	public void setEditable(boolean editable) {
+		this.editable = editable;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public Author getAuthor() {
+		return author;
+	}
+	public void setAuthor(Author author) {
+		this.author = author;
+	}
+	public LocalDateTime getDateCreated() {
+		return dateCreated;
+	}
+	public void setDateCreated(LocalDateTime dateCreated) {
+		this.dateCreated = dateCreated;
+	}
+	public LocalDateTime getDateUpdated() {
+		return dateUpdated;
+	}
+	public void setDateUpdated(LocalDateTime dateUpdated) {
+		this.dateUpdated = dateUpdated;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public List<BlogComment> getComments() {
+		return comments;
+	}
+	public void setComments(List<BlogComment> comments) {
+		this.comments = comments;
+	}
+	public int getPostId() {
+		return postId;
+	}
+	public void setPostId(int postId) {
+		this.postId = postId;
+	}
+	@Override
+	public String toString() {
+		return "BlogPost [postId=" + postId + ", title=" + title + ", author=" + author + ", dateCreated=" + dateCreated
+				+ ", dateUpdated=" + dateUpdated + ", content=" + content + ", comments=" + comments + "]";
+	}
+	
+	
+	
+	
 }
